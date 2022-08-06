4b43d2ee2ce2abd5706bb8017f617e98799a5a5d
Kartuesova <109457235+Kartuesova@users.noreply.github.com>
2022-08-06 21:28:45 +0300

    add Task3

---
+++ Example002_Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿int number1 = Понедельник ;
+int number2 = Вторник ;
+int number3 = Среда ;
+int number4 = Четверг ; 
+int number5 = Пятница ;
+int number6 = Суббота ;
+int number7 = Воскресение ;
+
+int size = 7;
+if number == 1 
+if (number1=size) size = number1 ;
+if (number2=size) size = number2 ;
+if (number3=size) size = Среда ;
+if (number4=size) size = Четверг;
+if (number5=size) size = Пятница ;
+if (number6=size) size = Суббота ;
+if (number7=size) size = Воскресенье ;
+Console.WriteLine(" size = ") ;
+Console.WriteLine( size ) ;
---
+++ Example003_Task 3/Program.cs
@@ -0,0 +1,12 @@
+﻿int a=3;
+int b=9;
+int c=5;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c  >max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
