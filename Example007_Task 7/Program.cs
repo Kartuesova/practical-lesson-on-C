9978b651287e3363d28026b772c4ccb76005e6dc
Kartuesova <109457235+Kartuesova@users.noreply.github.com>
2022-08-07 14:07:32 +0300

    add Example_Task 7

---
+++ Example007_Task 7/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число :");
+    while(true)
+    {
+        string input = Console.ReadLine();
+            Console.Write("{0} {1}",input, input[1]);
+            Console.WriteLine(" Вторая цифра трехзначного числа:");
+    }
