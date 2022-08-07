0b8fccf9d90558afd5dd654f278715375f610c26
Kartuesova <109457235+Kartuesova@users.noreply.github.com>
2022-08-07 13:47:57 +0300

    add Example_Task 6

--- Example002_Task 2/Program.cs
+++ Example002_Task 2/Program.cs
@@ -1,18 +1,66 @@
-﻿public enum Days
-{ 
-    Понедельник = 1,
-    Вторник = 2,
-    Среда = 3,
-    Четверг = 4, 
-    Пятница = 5,
-    Суббота = 6, 
-    Воскресение =7,
-}
+﻿namespace ConsoleApplication1
 
-if public static void main();
+{
+    enum Week
+    {
+        p1 = 1,
+        v2,
+        s3,
+        ch4,
+        p5,
+        s6,
+        v7
+    };
+    class program
+    {
+        static void Main(string[] args)
+    {
+        Console.WriteLine( "Введите число(от 1 до 7): ");
+        Week mb =Converter.ToInt16(Console.ReadLine());
+        TellAboutWeek(mb);
+    }
 
-{ 
-    int value =3;
-    string day = Enum.GetName(typeof(Days).Value);
-    Console.WriteLine(day);
-}
+        private static void TellAboutWeek(Week mb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Converter
+    {
+        internal static Week ToInt16(string? v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private static void TellAboutWeek(Week mb)
+         {
+          switch(mb)
+            {  
+                case Week.p1:
+                    Console.WriteLine("Понедельник");
+                    break;
+                case Week.v2:
+                    Console.WriteLine("Вторник");
+                    break;
+                case Week.s3:
+                    Console.WriteLine("Среда");
+                    break;
+                case Week.ch4:
+                    Console.WriteLine("Четверг");
+                    break;
+                case Week.p5:
+                    Console.WriteLine("Пятница");
+                    break;
+                case Week.s6:
+                    Console.WriteLine("Суббота");
+                    break;
+                case Week.v7:
+                    Console.WriteLine("Воскресенье");
+                    break;
+                
+            }
+        }
+    }
+
