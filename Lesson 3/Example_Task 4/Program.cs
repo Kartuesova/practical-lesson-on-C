e5d9c0e7c1102e2f53726e249bd86379a90bec6b
Kartuesova <109457235+Kartuesova@users.noreply.github.com>
2022-08-18 17:12:22 +0300

    add Lesson 3

---
+++ Lesson 3/Example_Task 4/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void Main(string[] args)
+{
+int i;
+int[] a = new int[8];
+Console.WriteLine("Введите массив чисел: ");
+for (i = 0; i < 8; i++)
+{
+a[i] = int.Parse(Console.ReadLine());
+}
+ 
+for (i = 0; i < 8; i++)
+{
+Console.Write($"{a[i]}");
+}
+}
+
+
+
+int N = 8;
+int min = 0;
+int max = 1;
+
+int[] CreateArray(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(CreateArray(N, min, max));
---
+++ Lesson 4/Example001/Program.cs
@@ -0,0 +1,37 @@
+﻿//Выяснить являются ли три числа сторонами треугольника
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a<b+c && b<a+c && c<a+b)
+{
+    Console.WriteLine("Это треугольник : ");
+
+}
+else 
+{
+ Console.WriteLine("Это не треугольник : ");
+}
+
+
+// второй вариант
+
+bool CheckTriangle(int a, int b, int c) => a+b>c && b+c>a && a+c>b;
+    
+    
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine() ?? "0");
+
+bool check = CheckTriangle(number1, number2, number3);
+Console.WriteLine(check ? "Три числа являются сторонами треугольника" :
+                    "Три числа не являются сторонами треугольника");
